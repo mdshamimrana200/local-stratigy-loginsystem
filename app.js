ee025c8c7c45ce3622e0b8631c132ef37241cf95
mdshamimrana200 <mdshamimrana20@gmail.com>
2024-04-03 14:40:17 +0600

    update

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const cors = require("cors");
 const path = require("path");
 
 require("./config/passport-local");
-require("ejs");
 require("./model/dbConnect");
 
-app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
